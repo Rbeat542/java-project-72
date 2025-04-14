9b945d425758e56f3c9997d367d37d408a07616d
Rbeat542 <a.makevnin@gmail.com>
2025-04-14 22:07:37 +0300

    app env structure remake

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -19,18 +19,21 @@ public class App {
         app.start(7070);
     }
 
-    public static String getDBUrl() throws SQLException {
+    public static String getDbUrl() throws SQLException {
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
         System.out.println("GET_ENB : " + dbUrl);
         if (null == dbUrl) {
             return "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;";
-        } else return "jdbc:postgresql://dpg-cvna5949c44c73dl3ik0-a:5432/hexlet_rpoject_db?password=19LkTLBf9Zdc1af6TZUOxHKXmQhY0Jov&user=hexlet_rpoject_db_user";
+        } else {
+            return "jdbc:postgresql://dpg-cvna5949c44c73dl3ik0-a:5432/hexlet_rpoject_db?password"
+                + "=19LkTLBf9Zdc1af6TZUOxHKXmQhY0Jov&user=hexlet_rpoject_db_user";
+        }
     }
 
     public static Javalin getApp() throws IOException, SQLException {
         var hikariConfig = new HikariConfig();
-        var DB_url = getDBUrl();
-        hikariConfig.setJdbcUrl(DB_url);
+        var dbUrl = getDbUrl();
+        hikariConfig.setJdbcUrl(dbUrl);
         var dataSource = new HikariDataSource(hikariConfig);
         var url = App.class.getClassLoader().getResourceAsStream("schema.sql");
         var sql = new BufferedReader(new InputStreamReader(url))
--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -1,40 +1,14 @@
 package hexlet.code.model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@AllArgsConstructor
+@Getter
+@Setter
 public class Url {
     private Integer id;
     private String name;
     private String createdAt;
 
-    public Url(Integer id, String name, String createdAt) {
-        this.id = id;
-        this.name = name;
-        this.createdAt = createdAt;
-    }
-
-    public Url() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
 }
