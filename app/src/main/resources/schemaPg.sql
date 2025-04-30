52e70c366c7350a27194bfc9b7fd217725c6532c
Rbeat542 <a.makevnin@gmail.com>
2025-05-01 02:50:54 +0300

    schema for PG fixed

--- app/src/main/resources/schemaPg.sql
+++ app/src/main/resources/schemaPg.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS urls CASCADE;
+DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
@@ -9,12 +9,13 @@ CREATE TABLE urls (
 
 DROP TABLE IF EXISTS url_checks;
 
-CREATE TABLE url_checks CASCADE(
+CREATE TABLE url_checks(
     id SERIAL PRIMARY KEY,
     status_code BIGINT NOT NULL,
     title VARCHAR(255) NOT NULL,
     h1 VARCHAR(200) NOT NULL,
     description VARCHAR(500) NOT NULL,
-    url_id BIGINT REFERENCES urls(id),
+    url_id BIGINT REFERENCES urls(id)
+    ON DELETE CASCADE,
     created_at TIMESTAMP
 );
