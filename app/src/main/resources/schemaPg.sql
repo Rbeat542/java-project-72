5a6a19f148b97866482673ac877ed0a25e4f98a4
Rbeat542 <a.makevnin@gmail.com>
2025-05-01 01:09:39 +0300

    schema for Postgress fix

--- app/src/main/resources/schemaPg.sql
+++ app/src/main/resources/schemaPg.sql
@@ -5,3 +5,16 @@ CREATE TABLE urls (
     name VARCHAR(100) NOT NULL,
     created_at TIMESTAMP
 );
+
+
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    status_code BIGINT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    h1 VARCHAR(200) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    url_id BIGINT REFERENCES urls(id),
+    created_at TIMESTAMP
+);
